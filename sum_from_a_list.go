0584586be44a5b9965574a86d0d3870a38139a47
Renju George <renjugeo@gmail.com>
2020-08-24 19:00:10 +0100

    given a list of numbers and k, return whether any two numbers add up to k

---
+++ sum_from_a_list.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Input
+	fmt.Println(pairExists([]int{10,15,3,7}, 17))
+        fmt.Println(pairExists([]int{1,2,3,4,5,6,7}, 13))
+        fmt.Println(pairExists([]int{1,2,3,4,5,6,7}, 14))
+}
+
+func pairExists(input []int, s int) bool {
+	var hash = make(map[int]bool)
+	for _, j := range input {
+		diff := s - j
+		_, ok := hash[diff]
+		if !ok {
+			hash[j] = true
+		} else {
+			return true
+		}
+	}
+	return false
+}
